eff1f2d8fc00129f3263bf0796f317ea3e606e11
Hiep Bui <hiep.bui@grvt.io>
2025-04-28 09:13:45 +0700

    feat: add tests files for local testing sdk

---
+++ tests/test.ts
@@ -0,0 +1,71 @@
+import { GrvtClient, GrvtEnvironment, Currency, TransferType } from '../src';
+
+const fundingAccountID = '';
+const tradingAccountId1 = '';
+
+async function testGetFundingAccountSummary(client: GrvtClient) {
+  try {
+    // Test getAccountSummary
+    console.log('Fetching account summary...');
+    const accountSummary = await client.getFundingAccountSummary();
+    console.log('Account Summary:', JSON.stringify(accountSummary, null, 2));
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+async function testGetSubAccountSummary(client: GrvtClient, tradingAccountId: string) {
+  try {
+    // Test getSubAccountSummary
+    console.log(`Fetching sub account summary of trading account ${tradingAccountId}...`);
+    const subAccountSummary = await client.getSubAccountSummary(tradingAccountId);
+    console.log(`Sub Account Summary of trading account ${tradingAccountId}:`, JSON.stringify(subAccountSummary, null, 2));
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+async function testTransfer(client: GrvtClient) {
+  const recipientAccountId = '';
+  // Test transfer
+  const transfer = await client.transfer({
+    from_account_id: fundingAccountID,
+    from_sub_account_id: '0',
+    to_account_id: recipientAccountId,
+    to_sub_account_id: '0',
+    currency: Currency.USDT,
+    num_tokens: '55',
+    transfer_type: TransferType.STANDARD,
+    transfer_metadata: '',
+  });
+
+  console.log('Transfer result:', JSON.stringify(transfer, null, 2));
+}
+
+async function main() {
+  // Load environment variables
+  const apiKey = {
+    apiKey: process.env.API_KEY,
+    apiSecret: process.env.API_SECRET
+  };
+  const env = GrvtEnvironment.DEV;
+
+  if (!apiKey.apiKey || !apiKey.apiSecret) {
+    console.error('Please set GRVT_API_KEY and GRVT_API_SECRET environment variables');
+    process.exit(1);
+  }
+
+  // Initialize the client
+  const client = new GrvtClient({
+    apiKey: apiKey.apiKey,
+    apiSecret: apiKey.apiSecret,
+    tradingAccountId: tradingAccountId1,
+    env
+  });
+
+  await testGetFundingAccountSummary(client);
+  await testGetSubAccountSummary(client, tradingAccountId1);
+  await testTransfer(client);
+}
+
+main().catch(console.error); 
