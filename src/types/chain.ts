e9e5b4bb08e178ab7f6abc7984be01207caacbd4
Bui Do Hiep <hiep.bui@grvt.io>
2025-04-30 15:26:27 +0700

    feat: add chain ID enum and restrict that in transfer metadata (#9)
    * feat: introduce new chain id enum and restrict transfer metadata with that
    
    * feat: bump version

--- src/types/chain.ts
+++ src/types/chain.ts
@@ -1,6 +1,22 @@
 export enum EChain {
-  ETH_MAINNET = 1,
-  ETH_SEPOLIA = 11155111,
-  ARBITRUM_ONE = 42161,
-  ARBITRUM_SEPOLIA = 421614,
+  // Mainnet
+  ARBITRUM_ONE = '42161',
+  BNB_SMART_CHAIN = '56',
+  BASE = '8453',
+  ETHEREUM = '1',
+  POLYGON = '137',
+  POLYGON_ZKEVM = '1101',
+  ZKSYNC_ERA = '324',
+  SOLANA = 'solana',
+  TRON = 'tron',
+
+  // Testnet
+  ARBITRUM_SEPOLIA = '421614',
+  BNB_TESTNET = '97',
+  BASE_SEPOLIA = '84532',
+  ETHEREUM_SEPOLIA = '11155111',
+  POLYGON_ZKEVM_TEST = '1442',
+  ZKSYNC_SEPOLIA = '300',
+  SOLANA_TESTNET = 'solana_test',
+  TRON_TESTNET = 'tron_test',
 }
