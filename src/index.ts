f8c2ad5ea3dfb6c7220825ca31de739f385dc393
danhlt <danh.danh20051995@gmail.com>
2025-04-30 12:17:54 +0700

    correct expose

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-export * from 'grvt';
+export * from '@grvt/client';
 export * from './client/GrvtClient';
 export * from './client/GrvtWsClient';
 export * from './config/config';
+export * from './signing/transfer';
+export * from './signing/utils';
+export * from './signing/withdraw';
+export * from './types/chain';
 export * from './types/deposit';
 export * from './types/transfer';
-export * from './types/chain';
-export * from './signing/transfer';
-export * from './signing/withdraw';
-export * from './signing/utils';
 
 // Re-export the main client class as default
 import { GrvtClient } from './client/GrvtClient';
